385ddedfe2c9816c5ff8e2eb27d9e813f4fcee01
bootcoding-developer <bootcoding.dev@gmail.com>
2023-08-16 09:55:53 +0530

    Get Mapping Types

--- coupon-system/src/main/java/com/bootcoding/spring/coupon/controller/CouponController.java
+++ coupon-system/src/main/java/com/bootcoding/spring/coupon/controller/CouponController.java
@@ -4,9 +4,7 @@ import com.bootcoding.spring.coupon.Coupon;
 import com.bootcoding.spring.coupon.service.CouponService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,14 +13,40 @@ import java.util.List;
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    // 1. Get All Coupons
+    @GetMapping("all-coupons")
+    public List<String> getAllCoupons(){
+        int quantity = 10;
+        return couponService.getNewCoupon(quantity);
+    }
+
+    // 2. Get Coupon Details by Coupon Id using PathVariable
+    @GetMapping("coupon-id/{id}")
+    public String getCouponById(@PathVariable("id") String couponId){
+        return couponId + "-" + couponService.newCoupon();
+    }
+
+    // 3. Get Coupons with Filter (Query Parameter)
+    @GetMapping("search_coupon")
+    public String searchCoupon(
+            @RequestParam("valid") boolean isValid,
+            @RequestParam(value = "type", required = false) String couponType,
+            @RequestParam("category") String category){
+        return "Query Parameter valid = " + isValid
+                + " | Coupon Type = " + couponType
+                + " | Coupon Category = " + category;
+    }
     // HTTP Methods Get
     @GetMapping("getCoupon")
     public String newCoupon(){
+        System.out.println("/getCoupon invoked");
         return "New Coupon : " + couponService.newCoupon();
     }
 
     @GetMapping("getJson")
     public Coupon getDummyJson(){
+        System.out.println("/getJson invoked");
         return Coupon.builder()
                 .id(couponService.newCoupon())
                 .type("Voucher")
@@ -31,6 +55,7 @@ public class CouponController {
 
     @GetMapping("getMultipleCoupons")
     public List<String> newCouponWithQuantity(){
+        System.out.println("/getMultipleCoupons invoked");
         int quantity = 10;
         return couponService.getNewCoupon(quantity);
     }
